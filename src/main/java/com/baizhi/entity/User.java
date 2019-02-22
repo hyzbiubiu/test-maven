38386cd4b6b2ab19ae30d8177e2ac8db2ab25f3e
hyz <775143433@qq.com>
2019-02-22 11:45:39 +0800

    change 2.0

---
+++ src/main/java/com/baizhi/entity/User.java
@@ -0,0 +1,55 @@
+package com.baizhi.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private Integer id;
+    private String username;
+
+    public User() {
+    }
+
+    public User(Integer id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
